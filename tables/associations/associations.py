93f64de7f8a490454e60ad0cf4b572369e367c47
Sasha <mouss42490@gmail.com>
2022-09-21 18:14:40 +0200

    Fix

--- tables/associations/associations.py
+++ tables/associations/associations.py
@@ -6,8 +6,8 @@ from engine.engine import MAIN_ENGINE
 job_offer_candidate_association = Table(
     "job_offer_candidate_association",
     Base.metadata,
-    Column("job_offer_id", Integer, ForeignKey("job_offers.class_id")),
-    Column("candidate_id", Integer, ForeignKey("candidate.user_id")),
+    Column("job_offer_id", Integer, ForeignKey("job_offers.id")),
+    Column("candidate_id", Integer, ForeignKey("candidate.id")),
 )
 
 
