436129de851b5554ac7376c40477a8d7af3feecd
Sasha <mouss42490@gmail.com>
2022-09-21 18:05:32 +0200

    some refactoring.

--- dev/create.py
+++ dev/create.py
@@ -2,16 +2,20 @@ import importlib
 import os
 
 tables = []
-search_dirs = ['tables']
+search_dirs = ["tables"]
 while search_dirs:
     search_dir = search_dirs.pop()
     basenames = os.listdir(search_dir)
-    paths = [os.path.join(search_dir, basename) for basename in basenames if '__pycache__' not in basename]
+    paths = [
+        os.path.join(search_dir, basename)
+        for basename in basenames
+        if "__pycache__" not in basename
+    ]
     for path in paths:
         if os.path.isdir(path):
             search_dirs.append(path)
         elif os.path.isfile(path):
-            path = path.replace('/', '.').strip('.py')
+            path = path.replace("/", ".").strip(".py")
             tables.append(path)
         else:
             raise ValueError(f"unknown path {path}")
--- sql_samples.py
+++ sql_samples.py
@@ -1,64 +1,64 @@
-def many_to_many():
+import os
+
+
+def test_many_to_many():
     from sqlalchemy.ext.declarative import declarative_base
     from sqlalchemy.orm import sessionmaker, relationship
     from sqlalchemy import Table, Column, ForeignKey, Integer, create_engine, String
 
     Base = declarative_base()
+
+    if os.path.exists("draft_engine"):
+        os.remove("draft_engine")
     draft_engine = create_engine("sqlite:///draft_engine", echo=True)
 
-    # association_table = Table(
-    #     "association",
-    #     Base.metadata,
-    #     Column("left_id", ForeignKey("left.id"), primary_key=True),
-    #     Column("right_id", ForeignKey("right.id"), primary_key=True),
-    # )
-    #
-    #
-    # class Parent(Base):
-    #     __tablename__ = "left"
-    #     id = Column(Integer, primary_key=True)
-    #     children = relationship("Child", secondary="association", backref="parents")
-    #
-    #
-    # class Child(Base):
-    #     __tablename__ = "right"
-    #     id = Column(Integer, primary_key=True)
-    #
-    #
-    # Base.metadata.create_all(draft_engine)
-    #
     Session = sessionmaker(bind=draft_engine)
     session = Session()
-    # session.add(Parent())
-    # b = session.query(Parent).filter().first()
-    # r = list(session.query(Child).filter())
-    # session.close()
 
-    student_identifier = Table('student_identifier',
-                                  Column('class_id', Integer, ForeignKey('classes.class_id')),
-                                  Column('user_id', Integer, ForeignKey('students.user_id'))
-                                  )
+    student_identifier = Table(
+        "student_identifier",
+        Base.metadata,
+        Column("class_id", Integer, ForeignKey("classes.class_id")),
+        Column("user_id", Integer, ForeignKey("students.user_id")),
+    )
 
-    class Student(Model):
-        __tablename__ = 'students'
+    class Student(Base):
+        __tablename__ = "students"
         user_id = Column(Integer, primary_key=True)
         user_fistName = Column(String(64))
         user_lastName = Column(String(64))
         user_email = Column(String(128), unique=True)
+        classes = relationship(
+            "Class",
+            secondary=student_identifier,
+            back_populates="students"
+        )
 
     class Class(Base):
-        __tablename__ = 'classes'
+        __tablename__ = "classes"
         class_id = Column(Integer, primary_key=True)
         class_name = Column(String(128), unique=True)
-        students = relationship("Student",
-                                   secondary=student_identifier)
+        students = relationship(
+            "Student",
+            secondary=student_identifier,
+            back_populates="classes"
+        )
+
+    Base.metadata.create_all(draft_engine)
 
     s = Student()
     c = Class()
     c.students.append(s)
+    s.classes.append(c)
     session.add(c)
+    session.add(s)
     session.commit()
 
+    class_sample = session.query(Class).first()
+    student_sample = session.query(Student).first()
 
-many_to_many()
+    assert len(student_sample.classes) == 1
+    assert len(class_sample.students) == 1
 
+
+test_many_to_many()
