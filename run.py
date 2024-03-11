56bb8303296812cb7d73537fbd5cb499070fc695
Sasha Keskin <sasha.keskin_ext@michelin.com>
2024-03-11 21:07:28 +0100

    Some check

--- run.py
+++ run.py
@@ -3,8 +3,7 @@ import os
 from flask_cors import CORS
 
 from app import app
-from methods.associations.job_offer_candidate_association import \
-    JobOfferCandidate
+from methods.associations.job_offer_candidate_association import JobOfferCandidate
 from methods.base import register_instance_methods
 from methods.candidate.candidate import Candidate
 from methods.professional.business import Business
@@ -13,6 +12,9 @@ from methods.professional.professional import Professional
 
 app.secret_key = os.urandom(12)
 
+for variable in ["GOOGLE_CREDENTIALS"]:
+    if not os.environ.get(variable):
+        raise RuntimeError(f"Environment variable {variable} is unset.")
 
 for _class in [Candidate, Professional, Business, JobOffers, JobOfferCandidate]:
     register_instance_methods(app, _class())
