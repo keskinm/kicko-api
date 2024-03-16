b3fd09ff8a6a02b298f6ff505ff17c9203bfacd7
Sasha Keskin <sasha.keskin_ext@michelin.com>
2024-03-16 21:03:34 +0100

    Configure alembic and init migration

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from database.base import Base
+from tables import tables_importer
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -14,11 +17,8 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
