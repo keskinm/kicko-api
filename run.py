529897f9d33e5fb87783ab67fe908d65b64f4f1b
Sasha Keskin <sasha.keskin_ext@michelin.com>
2024-03-17 22:56:09 +0100

    Refactoring

--- run.py
+++ run.py
@@ -2,8 +2,6 @@ import os
 
 from flask_cors import CORS
 from settings import LocalSettings
-from flask import request
-import json
 
 from app import app
 
@@ -11,33 +9,6 @@ LocalSettings(app)
 app.secret_key = os.urandom(12)
 CORS(app)
 
-
-@app.after_request
-def log_response(response):
-    os.makedirs("metrics", exist_ok=True)
-    if response.data and 'application/json' in response.content_type:
-        route_name = request.endpoint
-        try:
-            json_data = json.loads(response.data.decode('utf-8'))
-            file_path = f"metrics/{route_name}.json"
-
-            if os.path.exists(file_path):
-                with open(file_path, "r") as infile:
-                    try:
-                        existing_data = json.load(infile)
-                    except json.JSONDecodeError:
-                        existing_data = []
-            else:
-                existing_data = []
-
-            existing_data.append(json_data)
-            with open(file_path, "w") as outfile:
-                json.dump(existing_data, outfile)
-        except json.JSONDecodeError as e:
-            print(f"Erreur lors du décodage JSON pour la route {route_name}: {e}")
-    else:
-        print(f"Réponse non-JSON ou vide pour la route {request.endpoint}")
-    return response
 
 @app.route("/")
 def home():
--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 import os
+from flask import request
+import json
 
 from firebase_admin import credentials, exceptions, get_app, initialize_app
 from api.base import register_instance_methods
@@ -26,6 +28,35 @@ class LocalSettings(CommonSettings):
                 raise RuntimeError(f"Environment variable {variable} is unset.")
         cred = credentials.Certificate(os.environ.get("GOOGLE_CREDENTIALS"))
         initialize_app(cred, {"storageBucket": "kicko-b75db.appspot.com"})
+        self.register_after_request_hooks()
+
+    def register_after_request_hooks(self):
+        @self.app.after_request
+        def log_response(response):
+            os.makedirs("metrics", exist_ok=True)
+            if response.data and 'application/json' in response.content_type:
+                route_name = request.endpoint
+                try:
+                    json_data = json.loads(response.data.decode('utf-8'))
+                    file_path = f"metrics/{route_name}.json"
+
+                    if os.path.exists(file_path):
+                        with open(file_path, "r") as infile:
+                            try:
+                                existing_data = json.load(infile)
+                            except json.JSONDecodeError:
+                                existing_data = []
+                    else:
+                        existing_data = []
+
+                    existing_data.append(json_data)
+                    with open(file_path, "w") as outfile:
+                        json.dump(existing_data, outfile)
+                except json.JSONDecodeError as e:
+                    print(f"Erreur lors du décodage JSON pour la route {route_name}: {e}")
+            else:
+                print(f"Réponse non-JSON ou vide pour la route {request.endpoint}")
+            return response
 
 
 class TestSettings(CommonSettings):
