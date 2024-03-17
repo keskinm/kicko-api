cef1a6133dbe0424307142a4d4cc2126b03e618c
Sasha Keskin <sasha.keskin_ext@michelin.com>
2024-03-17 17:02:20 +0100

    Ongoing enrich database

--- test/enrich_database.py
+++ test/enrich_database.py
@@ -2,6 +2,9 @@
 
 from models.candidate.candidate import Candidate
 from models.professional.professional import Professional
+from models.professional.business import Business
+from models.professional.job_offers import JobOffers
+
 
 
 def fill_db_with_users(app):
@@ -26,6 +29,31 @@ def fill_db_with_users(app):
             country="France",
             zone="",
             phone_number="070000000",
+        )
+    )
+    app.db_session.commit()
+
+
+def fill_db_with_business(app):
+    fill_db_with_users(app)
+    pro_id = app.db_session.query(Professional).first().id
+    app.db_session.add(
+        Business(
+            professional_id=pro_id
+        )
+    )
+    app.db_session.commit()
+
+
+def fill_db_with_job_offers_and_appliers(app):
+    fill_db_with_business(app)
+    business_id = app.db_session.query(Business).first().id
+    app.db_session.add(
+        JobOffers(
+            name="12345",
+            description="professional",
+            requires="professional",
+            business_id=business_id,
         )
     )
     app.db_session.commit()
