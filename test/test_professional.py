a3c90b8b7a588cb55ac3ce462651b29b214c5b3a
Sasha Keskin <sasha.keskin_ext@michelin.com>
2024-03-17 15:17:31 +0100

    Refactoring

--- test/test_professional.py
+++ test/test_professional.py
@@ -15,7 +15,7 @@ def test_delete_candidate_account(
     test_client, auth_header, expc_status_code, expc_status
 ):
     with mock.patch("api.common.decode_auth_token") as mock_decode:
-        mock_decode.return_value = (True, "email@example.com")
+        mock_decode.return_value = (True, "toto@gmail.com")
 
         response = test_client.get(
             "/api/delete_candidate_account",
