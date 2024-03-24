e0332afb1002818be11bfce49baea8942ee7905d
Sasha Keskin <sasha.keskin_ext@michelin.com>
2024-03-24 18:03:47 +0100

    More robust path

--- database/database.py
+++ database/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from pathlib import Path
 
-DATABASE_URL = "sqlite:///main_engine.db"
+engine_path = Path(__file__).parent.parent / "main_engine.db"
+DATABASE_URL = f"sqlite:///{engine_path}"
 MAIN_ENGINE = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=MAIN_ENGINE)
